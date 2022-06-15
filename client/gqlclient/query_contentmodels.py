378edb9ffa52ed2b9344b2070481554dacd075c9
freyp567 <freyp567@gmail.com>
2022-06-15 20:45:17 +0200

    client using graphql query to list contentmodels

---
+++ client/gqlclient/query_contentmodels.py
@@ -0,0 +1,52 @@
+# graphql query to list content models
+
+import json
+import requests 
+
+API_URL = "https://dzl9t7sk9bsqh.cloudfront.net"
+READ_URL = API_URL + "/cms/read/de-DE"
+API_KEY = "a3b3604d9599f4da0a0e7abe2fbcbc8d9ae2e4badaa8411a"
+
+
+
+def main():
+    url = READ_URL
+
+    headers = {
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Bearer " + API_KEY,
+        "Connection": "keep-alive",
+        #User-Agent': 'query_contentmodels',
+        "Origin": API_URL
+    }
+    query = {
+        "query": "{listContentModels {data {name  modelId} }}"
+    }
+    query2 = {
+        "query": "{listBookitems {data {bookId bookTitle}}}"
+    }
+    #  error {code message}
+
+    session = requests.Session()
+    # need to enforce TLSv1.2 ? have TLSv1.3
+
+    response = session.post(
+        url = url,
+        headers = headers,
+        json = query2
+    )
+    if response.status_code != 200:
+        # We can't connect to the server for this app or website at this time.
+        print(f"request failed with status_code={response.status_code}\ndetails:\n\n")
+        print(response.text)
+        print("\n")
+        raise RuntimeError("request failed")
+
+    result = response.json()
+    print("\nquery result:\n\n")
+    print(json.dumps(result, indent=4))
+    print("done")
+
+main()
